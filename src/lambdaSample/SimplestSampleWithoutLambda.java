56cfff6bc4fcaf82198d7d633fc9a7a320cf887b
tada195908 <48470775+tada195908@users.noreply.github.com>
2022-05-15 11:45:22 +0900

    Add files via upload

---
+++ src/lambdaSample/SimplestSampleWithoutLambda.java
@@ -0,0 +1,32 @@
+package lambdaSample;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ *
+ * @author tadaki
+ */
+public class SimplestSampleWithoutLambda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n = 100;
+        List<Double> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(Math.random());
+        }
+        Consumer<Double> c = new Consumer<>(){
+            @Override
+            public void accept(Double d){
+                System.out.println(d);
+            }
+        };
+        //print all elements
+        list.stream().forEach(c);
+    }
+
+}
